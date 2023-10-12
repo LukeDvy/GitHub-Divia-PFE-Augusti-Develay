504708c97df7ecbe2ab7f7132db482415502f184
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-10-12 16:06:00 +0200

    Premières requêtes de base de donneés (python, CSV)
    Requête permettant d'afficher toutes les lignes de trajets possibles et leurs types associés (bus, tramway), pour une date associée

--- firstScriptGTFS.py
+++ firstScriptGTFS.py
@@ -12,15 +12,38 @@ trips=pd.read_csv('GTFS/trips.txt',delimiter=',')
 
 #print(agency.head(10))
 
-#1ere recherche : pour une date donnée, recherche des 
+#1ere recherche : pour une date donnée, recherche des trips et quelques détails
 date_cherche = '20231207'
 date_correct = calendar_dates[calendar_dates['date'].astype(str) == str(date_cherche)]
 print(date_correct)
-
-for index, row in date_correct.iterrows():
+'''route_retenue=pd.DataFrame()
+for index, row in date_correct.iterrows(): #lis chaque ligne à la date du 07/12/2023, et pour chaque service_id
     jour=calendar[row['service_id'] == calendar['service_id'].astype(str)]
-    route_id=trips[row['service_id'] == trips['service_id'].astype(str)]
-    if not jour.empty:
-        print(jour)
-    if not route_id.empty:
-        print(route_id)
+    route_retenue=trips[row['service_id'] == trips['service_id'].astype(str)]
+    
+    print("--------------------------------")
+    if not route_retenue.empty:
+    # Filtrer le DataFrame "routes" en utilisant route_retenue
+        routes_detail = routes[routes['route_id'].astype(str) == route_retenue['route_id'].astype(str).values[0]]
+        print(routes_detail)
+    else:
+        print("route_retenue est vide, impossible de filtrer routes_detail")'''
+
+# fusionne les df en utilisant la colonne 'service_id'
+new_base = pd.merge(trips, date_correct, on='service_id', how='inner')
+result = pd.merge(new_base, routes, on='route_id', how='inner')
+
+# séleectionne certaines colonnes souhaitées
+columns_to_display = ['route_id', 'route_short_name', 'route_long_name', 'route_type']
+
+# traduction de la colonne route_type de int à string : 0=Tram, 3=Bus
+result['route_type'] = result['route_type'].astype(str)
+for index, row in result.iterrows():
+    if str(row['route_type'])=="3":
+        result.loc[index, 'route_type'] = "Bus"
+    if str(row['route_type'])=="0":
+        result.loc[index, 'route_type'] = "Tramway"
+result = result[columns_to_display]
+
+# Afficher les résultats
+print(result.drop_duplicates())
