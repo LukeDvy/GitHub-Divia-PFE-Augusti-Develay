d9d3dfe3d27a2acdcdeb28f0fda727b3321978ff
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-10-13 10:17:03 +0200

    Première requête file : firstRealTime.py
    Création fonction prenant en entrée 'trip_id' et 'stop_id' et renvoie les informations sur l'arrêt souhaité.

--- firstRealTime.py
+++ firstRealTime.py
@@ -5,6 +5,15 @@ import urllib
 feed = gtfs_realtime_pb2.FeedMessage()
 response = urllib.request.urlopen('https://proxy.transport.data.gouv.fr/resource/divia-dijon-gtfs-rt-trip-update')
 feed.ParseFromString(response.read())
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
+
+def findStopById(target_trip_id:str,target_stop_id:str):
+    for entity in feed.entity:
+        if entity.HasField('trip_update'):
+            if entity.trip_update.trip.trip_id == target_trip_id:
+                for stop_time_update in entity.trip_update.stop_time_update:
+                    if stop_time_update.stop_id == target_stop_id:
+                        print("Arrêt trouvé pour le trip_id recherché:")
+                        print(stop_time_update)
+                        return stop_time_update
+
+findStopById("29-T2-14-1-100357","4-1459")
