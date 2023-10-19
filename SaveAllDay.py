4a7d3998778eb85defffea6795fe1b39613ee9d4
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-10-19 14:30:41 +0200

    Reformatage avec black des 3 files python

--- SaveAllDay.py
+++ SaveAllDay.py
@@ -8,55 +8,77 @@ import csv
 import os
 import time
 
+
 def SaveAllStopByDay():
-    #lecture du GTFS-RT
+    # lecture du GTFS-RT
     feed = gtfs_realtime_pb2.FeedMessage()
-    response = urllib.request.urlopen('https://proxy.transport.data.gouv.fr/resource/divia-dijon-gtfs-rt-trip-update')
+    response = urllib.request.urlopen(
+        "https://proxy.transport.data.gouv.fr/resource/divia-dijon-gtfs-rt-trip-update"
+    )
     feed.ParseFromString(response.read())
 
-    nomFichier = f'Trip_By_Day/{str(date.today())}.csv'
+    nomFichier = f"Trip_By_Day/{str(date.today())}.csv"
     newFichier = os.path.isfile(nomFichier)
 
-    with open(nomFichier, 'a', newline='') as fichier:
-        writer = csv.writer(fichier, delimiter=',')
+    with open(nomFichier, "a", newline="") as fichier:
+        writer = csv.writer(fichier, delimiter=",")
 
         # si nouveau fichier
         if not newFichier:
-            writer.writerow(['trip_id', 'stop_id', 'direction_id','arrival_delay','arrival_time','departure_delay','departure_time'])
+            writer.writerow(
+                [
+                    "trip_id",
+                    "stop_id",
+                    "direction_id",
+                    "arrival_delay",
+                    "arrival_time",
+                    "departure_delay",
+                    "departure_time",
+                ]
+            )
 
-        #listeTrip=affichageToutesLignesByDate('20231207')
-        trip_id=""
-        stop_id=""
-        direction_id=""
-        arrival_delay=""
-        arrival_time=""
-        departure_delay=""
-        departure_time=""
+        # listeTrip=affichageToutesLignesByDate('20231207')
+        trip_id = ""
+        stop_id = ""
+        direction_id = ""
+        arrival_delay = ""
+        arrival_time = ""
+        departure_delay = ""
+        departure_time = ""
         for entity in feed.entity:
-            if entity.HasField('trip_update'):
+            if entity.HasField("trip_update"):
                 for id_trip in entity.trip_update.stop_time_update:
-                    trip_id=str(entity.trip_update.trip.trip_id)
-                    stop_id=str(id_trip.stop_id)
-                    direction_id=str(entity.trip_update.trip.direction_id)
-                    arrival_delay=str(id_trip.arrival.delay)
-                    arrival_time=str(id_trip.arrival.time)
-                    departure_delay=str(id_trip.departure.delay)
-                    departure_time=str(id_trip.departure.time)
-                    
-                    #creation ligne
-                    nouvelle_ligne = [trip_id, stop_id, direction_id,arrival_delay,arrival_time,departure_delay,departure_time]
+                    trip_id = str(entity.trip_update.trip.trip_id)
+                    stop_id = str(id_trip.stop_id)
+                    direction_id = str(entity.trip_update.trip.direction_id)
+                    arrival_delay = str(id_trip.arrival.delay)
+                    arrival_time = str(id_trip.arrival.time)
+                    departure_delay = str(id_trip.departure.delay)
+                    departure_time = str(id_trip.departure.time)
+
+                    # creation ligne
+                    nouvelle_ligne = [
+                        trip_id,
+                        stop_id,
+                        direction_id,
+                        arrival_delay,
+                        arrival_time,
+                        departure_delay,
+                        departure_time,
+                    ]
                     # Ajouter la nouvelle ligne de données
                     writer.writerows([nouvelle_ligne])
 
-        #suppression des doublons sur les colonnes 'trip_id' et 'stop_id'
-        df = pd.read_csv(nomFichier, delimiter=',')
-        df = df.drop_duplicates(subset=['trip_id', 'stop_id'], keep='last')
-        df.to_csv(nomFichier, sep=',', index=False)
-    
+        # suppression des doublons sur les colonnes 'trip_id' et 'stop_id'
+        df = pd.read_csv(nomFichier, delimiter=",")
+        df = df.drop_duplicates(subset=["trip_id", "stop_id"], keep="last")
+        df.to_csv(nomFichier, sep=",", index=False)
+
     return "good"
 
-#appel de la fonction et écriture dans le fichier CSV toutes les 60 secondes
-while(True):
+
+# appel de la fonction et écriture dans le fichier CSV toutes les 60 secondes
+while True:
     SaveAllStopByDay()
     print("Enregistrement effectué")
     time.sleep(60)
