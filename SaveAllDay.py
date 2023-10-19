ed6254a9ce59fe0c5ccba6004c1386407407f539
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-10-19 13:57:01 +0200

    Fix suppression des doublons dans le fichier CSV
    Suppression des doublons sur les colonnes trip_id et stop_id en gardant la derniere ligne.

--- SaveAllDay.py
+++ SaveAllDay.py
@@ -11,19 +11,6 @@ feed = gtfs_realtime_pb2.FeedMessage()
 response = urllib.request.urlopen('https://proxy.transport.data.gouv.fr/resource/divia-dijon-gtfs-rt-trip-update')
 feed.ParseFromString(response.read())
 
-#fonction renvoyant la section JSON donnant les informations sur les horaires en fonction de l'id du trip à un arrêt précis
-def findStopById(target_trip_id:str,target_stop_id:str):
-    for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            if entity.trip_update.trip.trip_id == target_trip_id: #recherche de la section comportant l'id du trip souhaité
-                for stop_time_update in entity.trip_update.stop_time_update:
-                    if stop_time_update.stop_id == target_stop_id: #recherche de la sous-section comportant l'id de l'arrêt souhaité
-                        print("Arrêt trouvé pour le trip_id recherché:")
-                        print(stop_time_update)
-                        return stop_time_update
-                    
-findStopById("29-T2-14-1-100357","4-1459")
-
 def SaveAllStopByDay():
     nomFichier = f'Trip_By_Day/{str(date.today())}.csv'
     newFichier = os.path.isfile(nomFichier)
@@ -59,6 +46,10 @@ def SaveAllStopByDay():
                     # Ajouter la nouvelle ligne de données
                     writer.writerows([nouvelle_ligne])
 
+        #suppression des doublons sur les colonnes 'trip_id' et 'stop_id'
+        df = pd.read_csv(nomFichier, delimiter=',')
+        df = df.drop_duplicates(subset=['trip_id', 'stop_id'], keep='last')
+        df.to_csv(nomFichier, sep=',', index=False)
     
     return "good"
 
