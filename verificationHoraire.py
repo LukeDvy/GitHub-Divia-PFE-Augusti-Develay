608d6fbd65798c535715e2831284d9b2af16923f
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-10-30 12:05:04 +0100

    Creation Classe par Jour, variable "date" et dataframe avec colonnes intérressantes

--- verificationHoraire.py
+++ verificationHoraire.py
@@ -9,24 +9,26 @@ import numpy as np
 # Chargement des fichiers GTFS dans des df
 stops = pd.read_csv("GTFS/stops.txt", delimiter=",")
 routes = pd.read_csv("GTFS/routes.txt", delimiter=",")
-datas = pd.read_csv("Trip_By_Day/2023-10-26.csv", delimiter=",")
+datas_2023_10_26 = pd.read_csv("Trip_By_Day/2023-10-26.csv", delimiter=",")
+datas_2023_10_27 = pd.read_csv("Trip_By_Day/2023-10-27.csv", delimiter=",")
+datas_2023_10_28 = pd.read_csv("Trip_By_Day/2023-10-28.csv", delimiter=",")
 stop_times = pd.read_csv("GTFS/stop_times.txt", delimiter=",")
 
 
-def recupRouteId(tripId: str):
+def recupRouteId(tripId: str,data_in):
     trips = pd.read_csv("GTFS/trips.txt", delimiter=",")
-    result = pd.merge(datas, stops, on="stop_id", how="inner")
+    result = pd.merge(data_in, stops, on="stop_id", how="inner")
     trips = trips.drop(columns="direction_id")
     result = pd.merge(result, trips, on="trip_id", how="inner")
     result = pd.merge(result, routes, on="route_id", how="inner")
 
     result = result[result["trip_id"].astype(str) == str(tripId)]
-    print("Trajet " + str(result["route_id"].loc[1]))
+    print("Trajet " + str(result["route_id"].iloc[0]))
 
 
-def miseEnForme(routeId: str, tripId: str, directionId: int):
+def miseEnForme(routeId: str, tripId: str, directionId: int,data_in):
     trips = pd.read_csv("GTFS/trips.txt", delimiter=",")
-    result = pd.merge(datas, stops, on="stop_id", how="inner")
+    result = pd.merge(data_in, stops, on="stop_id", how="inner")
     trips = trips.drop(columns="direction_id")
     result = pd.merge(result, trips, on="trip_id", how="inner")
     result = pd.merge(result, routes, on="route_id", how="inner")
@@ -62,7 +64,7 @@ def miseEnForme(routeId: str, tripId: str, directionId: int):
         result.loc[index, "departure_time_reel"] = datetime.datetime.fromtimestamp(
             row["departure_time_reel"]
         )
-    print("Trajet " + str(result["route_long_name"].loc[1]))
+    print("Trajet " + str(result["route_long_name"].iloc[0]))
     result = result.sort_values(by="departure_time_reel")
     result = result.drop_duplicates(subset=["trip_id", "stop_id"], keep="last")
     for index, row in result.iterrows():
@@ -76,17 +78,56 @@ def miseEnForme(routeId: str, tripId: str, directionId: int):
         )
 
 
-#recupRouteId("10-L3-1-1-141100")
-#miseEnForme("4-L3", "10-L3-1-1-141100", 1)
+#recupRouteId("25-T1-1-A-045244",datas_2023_10_28)
+#miseEnForme("4-T1", "25-T1-1-A-045244", 0,datas_2023_10_28)
 
-def routeParTripParJour():
+#TRIP/ROUTE diff semaine & week-end
+#
+#
+
+class TripParJour:
+    def __init__(self,data,date):
+        self.data = data
+        self.date=date
+
+def routeParTripParJour(data_in):
+    date=str(datetime.datetime.fromtimestamp(data_in["departure_time"].iloc[0]))[:10]
+    print(date)
     trips = pd.read_csv("GTFS/trips.txt", delimiter=",")
     trips = trips.drop(columns="direction_id")
-    result = pd.merge(datas, trips, on="trip_id", how="inner")
+    result = pd.merge(data_in, trips, on="trip_id", how="inner")
     result = pd.merge(result, routes, on="route_id", how="inner")
-    result = result.drop(columns=["stop_id","direction_id","service_id","shape_id","trip_headsign","trip_short_name","agency_id","route_short_name","route_type"])
-    print(result.loc[0])
+    result = result.drop(columns=["arrival_time","departure_time","stop_id","direction_id","service_id","shape_id","trip_headsign","trip_short_name","agency_id","route_short_name"])
+    #columns restantes : [trip_id,arrival_delay,departure_delay,route_id,route_long_name]
 
+    agg_funcs = {
+    'trip_id': 'count',
+    'arrival_delay': 'mean',
+    'departure_delay': 'mean',
+    'route_long_name': 'first',
+    'route_type': 'first'
+    }
+
+    # Utilisez la méthode .groupby() pour regrouper par "route_id" et appliquer les opérations spécifiées.
+    result = result.groupby('route_id').agg(agg_funcs).reset_index()
+    result = result.rename(columns={'trip_id': 'trip_id_count'})
+    
+    # traduction de la colonne route_type de int à string : 0=Tram, 3=Bus
+    result["route_type"] = result["route_type"].astype(str)
+    for index, row in result.iterrows():
+        if str(row["route_type"]) == "3":
+            result.loc[index, "route_type"] = "Bus"
+        if str(row["route_type"]) == "0":
+            result.loc[index, "route_type"] = "Tramway"
+
+    print(result)
+    return TripParJour(result,date)
+
+df1=routeParTripParJour(datas_2023_10_27) # vendredi
+df2=routeParTripParJour(datas_2023_10_28) # samedi
+
+def diffSemaineEtWeekend(data_1,data_2):
+    print(data_1.date)
     return 0
 
-routeParTripParJour()
+diffSemaineEtWeekend(df1,df2)
